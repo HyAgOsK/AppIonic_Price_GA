b1ec4109dc54b44504399051fe36bdafa60741ac
hyago silva <hyagovieira@geb.inatel.br>
2024-01-30 11:37:17 -0300

    Adicionando arquivos finais

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,4 +9,24 @@ export class HomePage {
 
   constructor() {}
 
+  public resultado: String = 'Resultado'
+  public precoAlcool: Number = 0
+  public precoGasolina: Number = 0
+  calcular(){
+    if((this.precoAlcool && this.precoGasolina) &&  
+    (this.precoAlcool.valueOf() !=  0 && this.precoGasolina.valueOf() != 0)){
+      var pAlcool = parseFloat(this.precoAlcool.toString());
+      var pGasolina = parseFloat(this.precoGasolina.toString());
+  
+      var res = pAlcool/pGasolina
+      if(res >= 0.7){
+        this.resultado = 'Melhor utilizar Gasolina' 
+      }else{
+        this.resultado = 'Melhor utilizar o Alcool' 
+      }
+    }
+    else{
+      this.resultado = 'Preencha ambos campos, não pode ser zero'
+    }
+  }
 }
